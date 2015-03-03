3b01c119868ee4886ddf86dcd277b7caa13485d8
Erik <erikzenker@hotmail.com>
2015-03-03 09:26:39 +0100

    New mapping Random finished

--- src/gol.cc
+++ src/gol.cc
@@ -112,7 +112,7 @@ int gol(const unsigned nCells, const unsigned nTimeSteps ) {
     MyCave cave(graybat::pattern::GridDiagonal(height, width));
     
     // Distribute vertices
-    cave.distribute(graybat::mapping::Roundrobin());
+    cave.distribute(graybat::mapping::Random(1234));
     
     /***************************************************************************
      * Run Simulation
