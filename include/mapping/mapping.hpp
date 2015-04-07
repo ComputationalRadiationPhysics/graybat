6f7eba2d82b1293909a4ce144ff7610fe8e2c82c
Erik <erikzenker@hotmail.com>
2015-04-07 12:22:25 +0200

    Removed old mapping file from includes

--- include/mapping/mapping.hpp
+++ include/mapping/mapping.hpp
@@ -4,7 +4,6 @@
 #include <vector>    /* std::vector */
 #include <assert.h>  /* assert */
 #include <stdlib.h>  /* srand, rand */
-#include <metis.h>   /* idx_t, METIS_PartGraphKway */
 
 /*******************************************************************************
  *
