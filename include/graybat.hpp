67bba249aedd4597d039d67fd69227cd6679d917
Erik <erikzenker@hotmail.com>
2015-04-08 16:06:01 +0200

    First test with doxygen

--- include/graybat.hpp
+++ include/graybat.hpp
@@ -1,6 +1,21 @@
 #pragma once
 
 /**
+ * @mainpage 
+ *  
+ * Hello World
+ *
+ * @section Section 1
+ *
+ * @section Section 2
+ *
+ * @subsection Section 2.1
+ *
+ */
+
+
+
+/**
  * @brief This is an umbrella header that provides all necessary 
  *        header to configure the graybat communication library.
  *
